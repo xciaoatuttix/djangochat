41c959ee510b8aea806f5669566b6565323fba40
David Matthew Mattli <dmm@mattli.us>
2008-11-05 17:38:58 -0600

    remove dead directory

--- chat/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.template import Context, loader
-from django.core.urlresolvers import reverse
-
-def login_view(request):
-    template = loader.get_template('chat/login.html')
-    c = Context()
-    return HttpResponse(t.render(c))
-
